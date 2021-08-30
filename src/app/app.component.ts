8beb92be6e15c20b200fbe70c1fbee64f3d2854a
sametcelikbicak <sametcelikbicak@outlook.com>
2021-08-30 11:55:49 +0300

    Add service and get custom data implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component, VERSION } from '@angular/core';
+import { HttpService } from './services/http.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  title = `Angular ${VERSION.major} Custom Interceptor Usage`;
+export class AppComponent {
+  public title = `Angular ${VERSION.major} Custom Interceptor Usage`;
+  public customData: string;
+
+  constructor(private httpService: HttpService) {}
+
+  public getCustomData(): void {
+    this.httpService
+      .makeGetRequest('https://my-json-server.typicode.com/typicode/demo/posts')
+      .subscribe(response => (this.customData = JSON.stringify(response)));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
